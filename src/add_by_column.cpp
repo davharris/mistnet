fca3d1a2c5bf7cceb593ee6f726827e90cc9e63c
David Harris <harry491@gmail.com>
2013-07-06 18:59:43 -0700

    comment

--- src/add_by_column.cpp
+++ src/add_by_column.cpp
@@ -5,6 +5,7 @@ using namespace Rcpp;
 // [[Rcpp::export]]
 NumericMatrix rcpp_add_biases(NumericMatrix m, NumericVector v) {
   for(int i = 0; i< m.ncol(); i++){
+      // might be possible to speed this up with +=
       m(_, i) = m(_, i) + v[i];
     };
   return m;
