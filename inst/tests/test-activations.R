d488a753af029075a8a7778ce105ca776c0618e7
David J. Harris <davharris@ucdavis.edu>
2013-07-16 18:03:56 -0700

    more tests

--- inst/tests/test-activations.R
+++ inst/tests/test-activations.R
@@ -3,6 +3,8 @@ context("Activation functions")
 test_that("sigmoid produces accurate values", {
   x = matrix(seq(-10, 10, length = 1E4), ncol = 2)
   expect_equal(sigmoid(x), plogis(x))
+  expect_equal(sigmoid(Inf), plogis(Inf))
+  expect_equal(sigmoid(-Inf), plogis(-Inf))
 })
 
 test_that("rectify produces accurate values", {
