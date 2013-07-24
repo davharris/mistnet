683d154a446d0216b57e234fe226d0de3fc53821
David J. Harris <davharris@ucdavis.edu>
2013-07-24 14:59:54 -0700

    add bottleneck

--- inst/sample_network.R
+++ inst/sample_network.R
@@ -1,10 +1,12 @@
+devtools::load_all()
 library(ggplot2)
 load("../scrf/birds.Rdata")
 
 y = route.presence.absence[in.train, ]
 x = scale(env)[in.train, ]
 n.out = ncol(y)
-n.hid = 10
+n.hid = 50
+n.bottleneck = 10
 n.in = ncol(x)
 n = nrow(y)
 mini.n = 100
@@ -12,24 +14,23 @@ mini.n = 100
 lr = .05
 
 w1 = matrix(0, nrow = ncol(x), ncol = n.hid)
-w1[,] = runif(length(w1), -.001, .001)
+w1[,] = rt(length(w1), 2)/10
 
-w2 = matrix(0, nrow = n.hid, ncol = n.out)
-w2[,] = runif(length(w2), -.001, .001)
+w2 = matrix(0, nrow = n.hid, ncol = n.bottleneck)
+w2[,] = rt(length(w2), 2)/10
+
+w3 = matrix(0, nrow = n.bottleneck, ncol = n.out)
+w3[,] = runif(length(w3), -.01, .1)
+
 
 b1 = rep(0, n.hid)
-b2 = qlogis(colMeans(y))
-
-
-
+b3 = qlogis(colMeans(y))
 
 maxit = 10000
 
 errors = rep(NA, maxit/100)
-dw1 = 0
-dw2 = 0
 
-Rprof()
+#Rprof()
 
 for(i in 1:maxit){
   
@@ -38,19 +39,27 @@ for(i in 1:maxit){
   batch.y = y[in.batch, ]
   
   h = sigmoid(batch.x %*% w1 %plus% b1)
-  yhat = sigmoid(h %*% w2 %plus% b2)
+  bottleneck.h = h %*% w2
+  yhat = sigmoid(bottleneck.h %*% w3 %plus% b3)
   
   
-  delta2 = crossEntropyGrad(y = batch.y, yhat = yhat) * 
+  delta3 = crossEntropyGrad(y = batch.y, yhat = yhat) * 
     sigmoidGrad(s = yhat)
   
+  dw3 = matrixMultiplyGrad(
+    n.hid = n.bottleneck, 
+    n.out = n.out, 
+    delta = delta3,
+    h = bottleneck.h
+  )
+    
+  delta2 = delta3 %*% t(w3)
   dw2 = matrixMultiplyGrad(
     n.hid = n.hid, 
-    n.out = n.out, 
+    n.out = n.bottleneck, 
     delta = delta2,
     h = h
   )
-  b2 = b2 - colMeans(delta2) * lr
   
   delta1 = sigmoidGrad(s = h) * (delta2 %*% t(w2))
   dw1 = t(
@@ -63,13 +72,16 @@ for(i in 1:maxit){
   )
   
   b1 = b1 - colMeans(delta1) * lr
+  b3 = b3 - colMeans(delta3) * lr
   
-  dw1 = dw1/mini.n * lr - w1 * .01
+  dw1 = dw1/mini.n * lr - w1 * 1E-5
   w1 = w1 + dw1
   
   dw2 = dw2/mini.n * lr
-  w2 = w2 + t(dw2) - w2 * .00001
+  w2 = w2 + t(dw2) - w2 * 1E-4
   
+  dw3 = dw3/mini.n * lr
+  w3 = w3 + t(dw3) - w3 * 1E-4
   
   if(i%%100 == 0){
     error = crossEntropy(y = batch.y, yhat = yhat)
@@ -79,37 +91,26 @@ for(i in 1:maxit){
   if(is.na(dw1[[1]])){stop("NAs")}
 }
 
-Rprof(NULL)
-summaryRprof()
+# Rprof(NULL)
+# summaryRprof()
 
 plot(1:length(errors) * 100, errors, type = "l")
 
 
 
 h = sigmoid(scale(env) %*% w1 %plus% b1)
-yhat = sigmoid(h %*% w2 %plus% b2)
+bottleneck.h = h %*% w2
+yhat = sigmoid(bottleneck.h %*% w3 %plus% b3)
 
+
+color = predict(prcomp(bottleneck.h))[,1]
 qplot(
   latlon[,1], 
   latlon[,2], 
-  color = h[,5], 
+  color = color, 
   cex = 2
-) + coord_equal()
+) + coord_equal() + scale_color_gradient2()
 
 
--2 * mean(rowSums(dbinom(
-  route.presence.absence[in.test, ], 
-  prob = yhat[in.test, ],
-  size = 1, 
-  log = TRUE
-)))
 
-
-colnames(w2) = colnames(route.presence.absence)
-
-scaled.w = apply(w2, 2, function(x) x / sqrt(sum(x^2)))
-
-plot.x = scaled.w[1 , ]
-plot.y = scaled.w[2 , ]
-plot(plot.x, plot.y, type = "n")
-text(plot.x, plot.y, label = colnames(route.presence.absence))
+hist(w2 %*% w3)
