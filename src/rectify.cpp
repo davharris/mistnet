3f98db41e57900e8b415a738ff96f8b89634dbcd
David Harris <harry491@gmail.com>
2013-08-21 19:21:33 -0400

    rectify was overwriting its inputs

--- src/rectify.cpp
+++ src/rectify.cpp
@@ -4,9 +4,10 @@ using namespace Rcpp;
 
 // [[Rcpp::export]]
 NumericMatrix rectify(NumericMatrix m){
+  NumericMatrix out(m.nrow(), m.ncol());
   for(int i = 0; i< m.ncol(); i++){
-      m(_, i) = clamp(0, m(_, i), INFINITY);
+      out(_, i) = clamp(0, m(_, i), INFINITY);
     };
-  return m;
+  return out;
 }
   
