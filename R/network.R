9fef324e9942dbc60b3608c8e78545a55069f0d5
David Harris <harry491@gmail.com>
2013-08-21 12:30:06 -0400

    skeleton for network object with list of layers

---
+++ R/network.R
@@ -0,0 +1,46 @@
+network = setRefClass(
+  Class = "network",
+  fields = list(
+    x = "matrix",
+    y = "matrix",
+    layers = "list",
+    n.layers = "integer",
+    minibatch.size = "integer",
+    minibatch.ids = "integer",
+    loss = "function",
+    lossGradient = "function"
+  ),
+  methods = list(
+    newMinibatch = function(row.nums){
+      if(missing(row.nums)){
+        minibatch.ids <<- sample.int(minibatch.size, replace = FALSE)
+      }else{
+        # Should this check that length(row.nums) == minibatch.size?
+        minibatch.ids <<- row.nums
+      }
+    },
+    feedForward = function(){
+      layers[[1]]$forwardPass(x[minibatch.ids, ])
+      for(i in 2:n.layers){
+        layers[[i]]$forwardPass(layers[[i - 1]]$output)
+      }
+    },
+    backprop = function(){
+      layers[[n.layers]]$backwardPass(
+        lossGradient(y = y[minibatch.ids, ], yhat = layers[[n.layers]]$output)
+      )
+      for(i in (n.layers - 1):1){
+        layers[[i]]$backwardPass(layers[[i + 1]]$error.grad)
+      }
+    },
+    updateCoefficients = function(){
+      for(layer in layers){
+        layer$updateCoefficients()
+      }
+    },
+    predict = function(newdata){
+      feedForward(newdata)
+      return(output)
+    }
+  )
+)
