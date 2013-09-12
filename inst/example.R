2d3f5916c18f9c7ca6e12e46c7dfba286cee4bf7
David J. Harris <davharris@ucdavis.edu>
2013-09-11 17:13:44 -0700

    tweak example code

--- inst/example.R
+++ inst/example.R
@@ -2,11 +2,11 @@ devtools::load_all()
 load("birds.Rdata")
 library(fastICA)
 
-n.ranef = 3L
+n.ranef = 25L
 n.importance.samples = 25L
 
-n.layer1 = 20L
-n.layer2 = 10L
+n.layer1 = 50L
+n.layer2 = 20L
 
 minibatch.size = 50L
 
@@ -16,15 +16,15 @@ xx = f$S[in.train, ]
 net = mistnet(
   x = xx,
   y = route.presence.absence[in.train, ],
-  nonlinearity.names = c("rectify", "linear", "sigmoid"),
+  nonlinearity.names = c("rectify", "rectify", "sigmoid"),
   hidden.dims = c(n.layer1, n.layer2),
   priors = list(
-    gaussian.prior(mean = 0, var = .0001),
-    gaussian.prior(mean = 0, var = .0001),
-    gaussian.prior(mean = 0, var = .0001)
+    gaussian.prior(mean = 0, var = .001),
+    gaussian.prior(mean = 0, var = .001),
+    gaussian.prior(mean = 0, var = .01)
   ),
-  learning.rate = 1E-4,
-  momentum = .9,
+  learning.rate = 1E-3,
+  momentum = .5,
   loss = crossEntropy,
   lossGrad = crossEntropyGrad,
   minibatch.size = minibatch.size,
@@ -42,43 +42,46 @@ net$layers[[3]]$coefficients[,] = rt(length(net$layers[[3]]$coefficients), df = 
 
 net$fit(1)
 
-losses = numeric(1E3)
+losses = numeric(1E4)
 for(i in 1:(length(losses))){
   net$fit(1)
   #losses[i] = mean(rowSums(net$reportLoss()))
   if(i%%100 == 0){
-    for(layer in 1:net$n.layers){
-      net$momentum = min((1 + i / 10000) / 2, .99)
-      net$learning.rate = 2E-4 / (1 + 1E-5 * i) * (1 - net$momentum)
+    for(layer.num in 1:net$n.layers){
+      net$momentum = min((1 + i / 1000) / 2, .99)
+      net$learning.rate = 2E-3 / (1 + 1E-5 * i) * (1 - net$momentum)
+      
+      # Hack to keep rectified units alive
+      if(identical(net$layers[[layer.num]]$nonlinearity,rectify)){
+        for(hidden.num in 1:net$layers[[layer.num]]$coef.dim[[2]]){
+          if(max(net$layers[[layer.num]]$activations[,hidden.num,]) < 0){
+            net$layers[[layer.num]]$biases[[hidden.num]] = 
+              net$layers[[layer.num]]$biases[[hidden.num]] + .1
+          }
+        }
+      }
     }
     cat(i, "\n")
     net$layers[[3]]$prior$mean = 
       c(0 * net$layers[[3]]$coefficients + rowMeans(net$layers[[3]]$coefficients))
-    if(i %% 1000 == 0){
-      #plot(losses[1:1E4], type = "l")
-      #abline(h = mean(losses[(i-100):i]))
-    }
   }
 }
 #plot(losses, type = "l")
 
-zz = apply(net$layers[[3]]$coefficients, 2, function(x) x / sqrt(sum(x^2)))
+scaled.coefs = sapply(
+  1:net$layers[[3]]$coef.dim[[1]],
+  function(i){
+    net$layers[[3]]$coefficients[i, ] * sd(net$layers[[2]]$outputs[,i,])
+  }
+)
+zz = apply(scaled.coefs, 1, function(x) x / sqrt(sum(x^2)))
 z = crossprod(zz)
 dimnames(z) = list(colnames(route.presence.absence), colnames(route.presence.absence))
 head(sort(z[,"Yellow-headed Blackbird"], decreasing=TRUE), 11)[-1]
 head(sort(z[,"Veery"], decreasing=TRUE), 11)[-1]
 
-net$selectMinibatch(1:nrow(xx))
-net$feedForward(
-  cbind(
-    xx, 
-    net$ranefSample(nrow = net$minibatch.size, ncol = n.ranef)
-  ), 
-  1
-)
-
 #mean(rowSums(net$reportLoss()))
-#plot(prcomp(net$layers[[2]]$output))
+plot(prcomp(net$layers[[2]]$outputs[,,1]))
 
 
 net$selectMinibatch(1:nrow(f$S))
