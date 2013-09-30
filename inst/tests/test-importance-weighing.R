197c3e8dc7e994135a42c8b3613d43f382008b98
davharris <harry491@gmail.com>
2013-09-29 19:10:48 -0700

    somewhat more robust test for importance weighting

--- inst/tests/test-importance-weighing.R
+++ inst/tests/test-importance-weighing.R
@@ -1,15 +1,16 @@
 context("Importance weighing")
 
 test_that("Importance weighting works", {
-  nrow = 17
-  ncol = 29
+  nrow = 133
+  ncol = 237
   
   # rows are data points; columns are samples.
   # unscaled probabilities from a half-Cauchy should ensure some extreme weights
-  outcomes = abs(matrix(rt(nrow * ncol, df = 1), nrow = nrow, ncol = ncol))
+  outcomes = abs(matrix(rt(nrow * ncol, df = 1), nrow = nrow, ncol = ncol)) * 10
   true.probs = outcomes / rowSums(outcomes)
   
   # each row can have a different baseline log-probability
+  # Set a very high mean error (mean 100) to make sure it works in this area
   raw.errors = -log(true.probs) + rnorm(nrow, mean = 100)
   
   expect_equal(weighImportance(raw.errors), true.probs)
