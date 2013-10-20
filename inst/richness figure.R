23a03ea1c5a8e0022bfeed5860ce3f82bfcd799e
davharris <harry491@gmail.com>
2013-10-20 16:43:29 -0700

    working on richness figure...

--- inst/richness figure.R
+++ inst/richness figure.R
@@ -1,16 +1,22 @@
+library(poibin)
+
 set.seed(1)
 load("birds.Rdata")
-load("expected.values.Rdata")
+#load("expected.values.Rdata")
+expected.values = t(apply(prediction.array, 1, rowMeans))
+max.richness = ncol(route.presence.absence)
 
-richness = rowSums(route.presence.absence[in.test, ])
+richness = rowSums(route.presence.absence[in.train, ][!in.fold, ])
 expected.richness = rowSums(expected.values)
+
+# Gaussian approximation. Switch in qpoibin later
 richness.sd = sqrt(
   rowSums(expected.values * (1 - expected.values))
 )
 
-lower.ci = expected.richness - qnorm(.999) * richness.sd
-upper.ci = expected.richness + qnorm(.999) * richness.sd
-outside.ci = ((richness < upper.ci) & (richness > lower.ci))
+lower.ci = expected.richness - qnorm(.975) * richness.sd
+upper.ci = expected.richness + qnorm(.975) * richness.sd
+inside.ci = ((richness < upper.ci) & (richness > lower.ci))
 
 plot(
   expected.richness, 
@@ -18,7 +24,11 @@ plot(
   cex = .6, 
   asp = 1, 
   xlim = range(c(lower.ci, upper.ci)), 
-  ylab = "observed.richness",
-  pch = ifelse(outside.ci, 1, 19)
+  ylab = "observed.richness"#,
+  #pch = ifelse(inside.ci, 1, 19)
 )
 abline(0,1)
+abline(mean(richness.sd) * 1.96, 1, lty = 2)
+abline(mean(richness.sd) * -1.96, 1, lty = 2)
+
+richness.sd + 
