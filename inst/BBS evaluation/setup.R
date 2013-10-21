91da2069f796baef6c3658ce29467e834c33c3da
davharris <harry491@gmail.com>
2013-10-20 18:32:44 -0700

    tweak some setup priors

--- inst/BBS evaluation/setup.R
+++ inst/BBS evaluation/setup.R
@@ -1,6 +1,12 @@
 devtools::load_all()
 load("birds.Rdata")
 
+# Triangular distribution PDF
+triangle = function(min, max){
+  unscaled = max:min - min + 1
+  unscaled / sum(unscaled)
+}
+
 
 # Build CV folds ----------------------------------------------------------
 # Setting a RNG seed so that I can ensure that all methods see the same CV 
@@ -31,8 +37,8 @@ prior.var1 = 10^(runif(1, min = -4, max = 0))
 prior.var2 = 10^(runif(1, min = -4, max = 0))
 prior.var3 = 10^(runif(1, min = -4, max = 0))
 
-# minibatch.size distributed uniformly between 10 and 100
-minibatch.size = sample(10:100, 1)
+# minibatch.size distributed triangularly between 10 and 100
+minibatch.size = sample(10:100, 1, prob = triangle(10, 100))
 
 # n.ranef distributed uniformly between 10 and 50
 n.ranef = sample(10:50, 1)
@@ -40,12 +46,8 @@ n.ranef = sample(10:50, 1)
 # n.importance.samples distributed uniformly between 10 and 50
 n.importance.samples = sample(10:50, 1)
 
-# n.layer2 triangularly distributed between 3 and 50
-triangle = function(min, max){
-  unscaled = max:min - min + 1
-  unscaled / sum(unscaled)
-}
-n.layer2 = sample(3:50, 1, prob = triangle(3, 50))
+# n.layer2 triangularly distributed between 5 and 25
+n.layer2 = sample(5:25, 1, prob = triangle(5, 25))
 
 # n.layer1 uniformly distributed between n.layer2 and 50
 n.layer1 = sample(n.layer2:50, 1)
