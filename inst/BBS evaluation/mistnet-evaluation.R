3e37f9b5a1b1446341ba8309f05bbb6f1ff7a2b8
David J. Harris <davharris@ucdavis.edu>
2013-11-11 12:36:24 -0800

    add stuff to mistnet evaluation

--- inst/BBS evaluation/mistnet-evaluation.R
+++ inst/BBS evaluation/mistnet-evaluation.R
@@ -1,6 +1,7 @@
 load("birds.Rdata")
 devtools::load_all()
 load("fold.ids.Rdata")
+library(plyr)
 
 num.attempts = 15L
 
@@ -21,11 +22,33 @@ for(attempt.num in 1:num.attempts){
     starting.rate = starting.rate
   )
   
+  out = cbind(hyperparameters, attempt.num = attempt.num, output.df)
   source("inst/example.R")
   save(
-    cbind(hyperparameters, output.df), 
+    out, 
     file = paste0("mistnet performance ", attempt.num, ".Rdata")
   )
 }
+
+results = do.call(
+  rbind,
+  lapply(
+    dir(pattern = "mistnet performance.*\\.Rdata"),
+    function(x){load(x);out}
+  )
+)
+
+means = ddply(results, c("seconds", "attempt.num"), function(x) mean(x$loglik))
+
+optimal.seconds = means$seconds[which.max(means$V1)]
+optimal.attempt.num = means$attempt.num[which.max(means$V1)]
+
+optimal.row = which(
+  optimal.attempt.num == results$attempt.num & 
+    optimal.seconds == results$seconds & 
+    results$fold.id == 1
+)
+
+results[optimal.row, ]
 
 print(Sys.time() - total.time.start)
