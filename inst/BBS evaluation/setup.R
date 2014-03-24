6b95ec009faa7f496c508ac108978c76f22864e7
davharris <harry491@gmail.com>
2014-03-23 20:54:58 -0700

    aim toward faster gradient estimates; get rid of more silly comments

--- inst/BBS evaluation/setup.R
+++ inst/BBS evaluation/setup.R
@@ -1,22 +1,16 @@
-# minibatch.size distributed uniformly between 10 and 100
 # Smaller minibatches allow for more gradient updates, bigger ones are more
 # precise.
-# I think the 10-100 range comes from Hinton's "Practical Guide" to RBMs.
-minibatch.size = sample(10:100, 1)
+minibatch.size = sample(10:50, 1)
 
-# n.ranef distributed uniformly
 n.ranef = sample(5:25, 1)
 
-# n.importance.samples distributed uniformly
 # Tang and Salakhutdinov seemed to find the best performance with 20-30 samples.
-n.importance.samples = sample(20:50, 1)
+n.importance.samples = sample(20:30, 1)
 
-# n.layer2 uniformly distributed
 # This layer does dimensionality reduction. With better priors, fewer dimensions
 # would probably be needed, since there still tend to be strong correlations.
 n.layer2 = sample(5:20, 1)
 
-# n.layer1 uniformly distributed
 # This layer does basis expansion, so it should be at least as wide as layer2,
 # which does dimensionality reduction
 n.layer1 = sample(n.layer2:50, 1)
