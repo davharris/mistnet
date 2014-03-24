779c75ed7d3aafc955f7f8bbcf4a234d560673d3
davharris <harry491@gmail.com>
2014-03-23 19:47:05 -0700

    it’s too easy for these comments to get out of date

--- inst/BBS evaluation/setup.R
+++ inst/BBS evaluation/setup.R
@@ -4,19 +4,19 @@
 # I think the 10-100 range comes from Hinton's "Practical Guide" to RBMs.
 minibatch.size = sample(10:100, 1)
 
-# n.ranef distributed uniformly between 5 and 25:
+# n.ranef distributed uniformly
 n.ranef = sample(5:25, 1)
 
-# n.importance.samples distributed uniformly between 10 and 50.
+# n.importance.samples distributed uniformly
 # Tang and Salakhutdinov seemed to find the best performance with 20-30 samples.
 n.importance.samples = sample(20:50, 1)
 
-# n.layer2 uniformly distributed between 5 and 20.
+# n.layer2 uniformly distributed
 # This layer does dimensionality reduction. With better priors, fewer dimensions
 # would probably be needed, since there still tend to be strong correlations.
 n.layer2 = sample(5:20, 1)
 
-# n.layer1 uniformly distributed between n.layer2 and 50
+# n.layer1 uniformly distributed
 # This layer does basis expansion, so it should be at least as wide as layer2,
 # which does dimensionality reduction
 n.layer1 = sample(n.layer2:50, 1)
