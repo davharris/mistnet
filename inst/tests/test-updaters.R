f6c88890a3bcbe55e56b1237f4f1dd1c3f47f611
davharris <harry491@gmail.com>
2014-04-27 21:33:35 -0700

    add barebones tests for updaters
    I need to be more consistent about the signs of gradients and deltas...

---
+++ inst/tests/test-updaters.R
@@ -0,0 +1,69 @@
+context("Updaters")
+
+test_that("sgd updater works",{
+  
+  set.seed(1)
+  coefs = matrix(0, 10, 10)
+  true.coefs = structure(rnorm(length(coefs)), dim = dim(coefs))
+  
+  updater = new(
+    "sgd.updater", 
+    delta = coefs, 
+    momentum = .9,
+    learning.rate = .01
+  )
+  
+  for(i in 1:1000){
+    updater$computeDelta(coefs - true.coefs)
+    coefs = coefs + updater$delta
+  }
+  
+  expect_equal(coefs, true.coefs) 
+})
+
+
+test_that("adagrad works",{
+  
+  set.seed(1)
+  coefs = matrix(0, 10, 10)
+  true.coefs = structure(rnorm(length(coefs)), dim = dim(coefs))
+  
+  updater = new(
+    "adagrad.updater", 
+    delta = coefs, 
+    squared.grad = coefs,
+    learning.rate = 1
+  )
+  
+  for(i in 1:1000){
+    updater$computeDelta(true.coefs - coefs)
+    coefs = coefs + updater$delta
+  }
+  
+  expect_equal(coefs, true.coefs)
+  
+})
+
+test_that("adadelta works",{
+  
+  set.seed(1)
+  coefs = matrix(0, 10, 10)
+  true.coefs = structure(rnorm(length(coefs)), dim = dim(coefs))
+  
+  updater = new(
+    "adadelta.updater", 
+    delta = coefs, 
+    rho = .95, 
+    epsilon = 1E-6, 
+    squared.grad = coefs, 
+    squared.delta = coefs
+  )
+  
+  for(i in 1:1000){
+    updater$computeDelta(coefs - true.coefs)
+    coefs = coefs + updater$delta
+  }
+  
+  expect_equal(coefs, true.coefs)
+  
+})
