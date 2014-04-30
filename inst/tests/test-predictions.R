92937c32d320b1f518f65eca6fa0fe47c97247f7
davharris <harry491@gmail.com>
2014-04-29 20:37:23 -0700

    more prediction expectations

--- inst/tests/test-predictions.R
+++ inst/tests/test-predictions.R
@@ -20,15 +20,37 @@ test_that("Prediction works",{
     n.ranef = 3L,
     ranefSample = gaussianRanefSample,
     n.importance.samples = 10L,
-    minibatch.size = 10L,
+    minibatch.size = nrow(x),
     training.iterations = 0L,
     loss = crossEntropy,
     lossGrad = crossEntropyGrad
   )
+  net$fit(1) # Feed forward
+  net$layers[[3]]$biases[] = 0 # Undo bis updates
   
   p = predict(net, rbind(x, x), n.importance.samples = n.importance.samples)
   
   expect_equal(dim(p), c(2 * nrow(x), ncol(y), n.importance.samples))
   
   expect_true(all(p == 0.5)) # With no coefficients, everything should be 0.5
+  
+  # With these settings, prediction should just make a copy (except for latent
+  # variables)
+  copy = predict(
+    net, 
+    x, 
+    n.importance.samples = net$n.importance.samples, 
+    return.model = TRUE
+  )
+  
+  copy2 = net$copy()
+  
+  
+  # Everything but layers should be identical
+  for(name in names(network$fields())){
+    if(name != "layers"){
+      expect_identical(copy$field(name), copy2$field(name))
+    }
+  }  
+  
 })
