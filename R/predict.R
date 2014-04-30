0aca2d4d5b4c20d11da7f3753337d996b14b59e1
davharris <harry491@gmail.com>
2014-04-29 18:04:43 -0700

    new (untested) predict function using $copy

--- R/predict.R
+++ R/predict.R
@@ -1,25 +1,13 @@
 predict.network = function(
   object, 
   newdata, 
-  n.importance.samples = 1000L
+  n.importance.samples = 1000L, 
+  return.model = FALSE
 ){
-  
-  # For some reason, the layers weren't copying by default--only the 
-  # reference is being transferred. 
-  # As a result, changes to `cpy$layers` were overwriting `object$layers`, 
-  # even after exiting the `predict` function.
-  # The next couple of lines are designed to prevent that behavior.
-  original.layers = lapply(object$layers, function(x) x$copy())
-  on.exit({
-    object$layers = original.layers
-  })
   
   cpy = object$copy(shallow = FALSE)
   cpy$n.importance.samples = n.importance.samples
-  # ensure that selectMinibatch triggers its extra routines for changing
-  # the number of importance samples.
-  # In retrospect, this is a screwy way to control things.
-  cpy$selectMinibatch(1L)
+  
   cpy$selectMinibatch(1:nrow(newdata))
   
   # Create an input matrix with the correct dimensions & correct values
@@ -31,6 +19,7 @@ predict.network = function(
   
   
   for(i in 1:cpy$n.importance.samples){
+    # Update the unobserved columns that occur after ncol(newdata)
     inputs[, -(1:ncol(newdata))] = cpy$ranefSample(
       nrow = nrow(newdata), 
       ncol = n.ranef
@@ -41,5 +30,9 @@ predict.network = function(
     )
   }
 
-  return(cpy$layers[[length(cpy$layers)]]$outputs)
+  if(return.model){
+    cpy
+  }else{
+    cpy$layers[[length(cpy$layers)]]$outputs
+  }
 }
