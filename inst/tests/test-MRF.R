7dfeb5358dd204b8c632d3cca7ff739011e1407e
davharris <davharris@ucdavis.edu>
2014-07-01 18:39:08 -0700

    remove unneeded `suggests` from description

--- inst/tests/test-MRF.R
+++ inst/tests/test-MRF.R
@@ -82,7 +82,7 @@ test_that("findPredictedCrossprod works", {
     runif(nrow * ncol * n.importance.weights),
     dim = c(nrow, ncol, n.importance.weights)
   )
-  importance.weights = gtools::rdirichlet(nrow, rep(1, n.importance.weights))
+  importance.weights = matrix(runif(n.importance.weights * nrow), nrow = nrow)
   
   x = findPredictedCrossprod(predicted, importance.weights)
   
