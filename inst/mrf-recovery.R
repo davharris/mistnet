87cdac7625063b7dcf2a6fddb65348042f99f0bc
davharris <davharris@ucdavis.edu>
2014-07-28 13:21:26 -0700

    update MRF recovery

--- inst/mrf-recovery.R
+++ inst/mrf-recovery.R
@@ -1,106 +1,96 @@
+set.seed(1)
+
 devtools::load_all()
 load("inst/fakedata.Rdata")
 
-n.importance.samples = 35L
-n.minibatch = 11L
-n.ranef = 4L
-learning.rate = .1
+scale = mean(abs(lateral[upper.tri(lateral)]))
 
-net = network$new(
+net = mistnet(
   x = env[ , 1:3],
   y = fakedata,
-  layers = list(
-    createLayer(
-      n.inputs = 3L + n.ranef,
-      n.outputs = ncol(fakedata),
-      prior = gaussian.prior$new(mean = 0, var = 1),
-      n.minibatch = n.minibatch,
-      n.importance.samples = n.importance.samples,
-      nonlinearity.name = "sigmoid",
-      updater.name = "adagrad",
-      updater.arguments = list(
-        learning.rate = learning.rate
-      )
+  layer.definitions = list(
+    defineLayer(
+      nonlinearity = mf_mrf.nonlinearity(
+        lateral = matrix(0, nrow = ncol(fakedata), ncol = ncol(fakedata)),
+        maxit = 50L,
+        damp = 0.2,
+        tol = 1E-4,
+        updater = new(
+          "adagrad.updater",
+          delta = matrix(0, nrow = ncol(fakedata), ncol = ncol(fakedata)),
+          learning.rate = .05
+        ),
+        l1.decay = 1 / scale / nrow(env)
+      ),
+      size = ncol(fakedata),
+      prior = gaussian.prior(mean = 0, sd = .5)
     )
   ),
-  n.layers = 1L,
-  dataset.size = nrow(env),
-  n.minibatch = n.minibatch,
-  n.importance.samples = n.importance.samples,
-  loss = crossEntropy,
-  lossGradient = crossEntropyGrad,
-  ranefSample = gaussianRanefSample,
-  n.ranef = n.ranef,
-  completed.iterations = 0L
-)
-
-# reset/initialize layer state
-net$layers[[1]]$resetState(n.minibatch, n.importance.samples)
-
-# initialize biases
-net$layers[[1]]$biases = matrix(qlogis(colMeans(fakedata)), nrow = 1)
-
-
-# Fill in all the stuff for the MRF nonlinearity
-scale = mean(abs(lateral[upper.tri(lateral)]))
-net$layers[[1]]$nonlinearity = mf_mrf.nonlinearity(
-  lateral = matrix(0, nrow = ncol(fakedata), ncol = ncol(fakedata)),
-  maxit = 50L,
-  damp = 0.2,
-  tol = 1E-4,
-  updater = new(
-    "adagrad.updater",
-    delta = matrix(0, nrow = ncol(fakedata), ncol = ncol(fakedata)),
-    learning.rate = learning.rate
-  ),
-  l1.decay = 1 / scale / nrow(env)
+  n.minibatch = 10L,
+  loss = bernoulliLoss(),
+  updater = adagrad.updater(learning.rate = .1),
+  initialize.biases = TRUE, 
+  initialize.weights = TRUE
 )
 
 
 
 
-for(iter in 1:5){
-  new.order = sample.int(nrow(net$x))
-  net$x = net$x[new.order, ]
-  net$y = net$y[new.order, ]
-  for(i in 1:50){
-    if(i%%50 == 0){
-      cat("\n")
-    }
-    cat(".")
-    net$fit(10)
-    if(is.nan(net$layers[[1]]$outputs[[1]])){
-      stop("NaNs detected :-(")
-    }
+par(mfrow = c(1, 2))
+for(i in 1:25){
+  net$fit(100)
+  if(any(is.na(net$layers[[1]]$nonlinearity$lateral))){
+    stop()
   }
+  
+  plot(
+    net$layers[[1]]$weights[1:3, ], 
+    coefs[1:3, ], 
+    asp = 1,
+    main = net$completed.iterations
+  )
+  abline(0,1)
+  plot(
+    net$layers[[1]]$nonlinearity$lateral,
+    lateral, 
+    cex = .8,
+    col = "#00000020",
+    pch = 16,
+    asp = 1
+  )
+  abline(0,1)
+  abline(0,0)
+  abline(v = 0)
 }
 
+pcs = predict(prcomp(t(net$layers[[1]]$weights[4:(3 + 10), ])))
 
+summary(lm(coefs[1, ] ~ 0 + net$layers[[1]]$weights[1, ]))
+summary(lm(coefs[2, ] ~ 0 + net$layers[[1]]$weights[2, ]))
+summary(lm(coefs[3, ] ~ 0 + net$layers[[1]]$weights[3, ]))
 
+summary(lm(coefs[4, ] ~ PC1+PC2, data = as.data.frame(pcs)))
+summary(lm(coefs[5, ] ~ PC1+PC2, data = as.data.frame(pcs)))
 
-plot(net$layers[[1]]$coefficients[1:3, ], coefs[1:3, ], asp = 1); abline(0,1)
-plot(
-  net$layers[[1]]$nonlinearity$lateral,
-  lateral, 
-  cex = .8,
-  col = "#00000020",
-  pch = 16
-)
-abline(0,1)
-abline(0,0)
-abline(v = c(-.005, .005))
-
-summary(lm(coefs[4, ] ~ 0 + t(net$layers[[1]]$coefficients[4:(3 + n.ranef), ])))
-summary(lm(coefs[5, ] ~ 0 + t(net$layers[[1]]$coefficients[4:(3 + n.ranef), ])))
-
-square = function(x){x^2}
-1-mean(square(net$layers[[1]]$nonlinearity$lateral - lateral))/mean(square(lateral - mean(lateral)))
+summary(lm(c(lateral) ~ 0+c(net$layers[[1]]$nonlinearity$lateral)))
 
 mean(abs(net$layers[[1]]$nonlinearity$lateral))
 
 
-
-summary(
-  lm(
-    c(lateral) ~ c(net$layers[[1]]$nonlinearity$lateral))
+par(mfrow = c(1, 1))
+plot(
+  net$layers[[1]]$nonlinearity$lateral,
+  lateral, 
+  asp = 1,
+  type = "n"
+)
+abline(0,1, lwd  = 2, col = "darkgray")
+abline(0, 0, lwd = 2, col = "darkgray")
+abline(v = 0, lwd = 2, col = "darkgray")
+points(
+  net$layers[[1]]$nonlinearity$lateral,
+  lateral,
+  cex = .5,
+  pch = 16,
+  col = "#00000040"
 )
